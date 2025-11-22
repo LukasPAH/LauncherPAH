1174cfed1955c26921c0eb037ea1775cc40e112b
Lukas <32801643+LukasPAH@users.noreply.github.com>
2025-11-22 14:57:51 -0600

    squirrel -> wix

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,5 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerWix } from "@electron-forge/maker-wix";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { FuseV1Options, FuseVersion } from "@electron/fuses";
@@ -13,7 +10,24 @@ const config: ForgeConfig = {
         icon: "images/icon.ico",
     },
     rebuildConfig: {},
-    makers: [new MakerSquirrel({}), new MakerZIP({}, ["darwin"]), new MakerRpm({}), new MakerDeb({})],
+    makers: [
+        new MakerWix({
+            manufacturer: "LukasPAH",
+            name: "LauncherPAH",
+            language: 1033, // English
+            appUserModelId: "LauncherPAH",
+            shortName: "LauncherPAH",
+            description: "Minecraft GDK Launcher",
+            icon: __dirname + "\\images\\icon.ico",
+            arch: "x64",
+            defaultInstallMode: "perMachine",
+            associateExtensions: "mcpack,mcworld",
+            bundled: true,
+            ui: {
+                chooseDirectory: true
+            }
+        }),
+    ],
     plugins: [
         new VitePlugin({
             // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
