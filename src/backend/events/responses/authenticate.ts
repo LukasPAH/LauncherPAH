1babd687ecf4e4920908dcbc5060c1c1fecb4e93
LukasPAH <lukas4460@gmail.com>
2026-05-09 15:34:57 -0500

    feat: add xbox auth function for linux (WIP integration with wine to come later)

---
+++ src/backend/events/responses/authenticate.ts
@@ -0,0 +1,76 @@
+import { BrowserWindow } from "electron";
+import { sleep } from "../../utils/sleep";
+
+interface IDeviceCodeResponse {
+    user_code: string;
+    device_code: string;
+    verification_uri: string;
+    interval: number;
+    expires_in: number;
+}
+
+interface IOAuthResponse {
+    token_type: string;
+    expires_in: number;
+    scope: string;
+    access_token: string;
+    refresh_token: string;
+    user_ud: string;
+}
+/**
+ *
+ * @returns The refresh token after authenticating, or undefined if unable to authenticate.
+ */
+export async function authenticate(): Promise<string | undefined> {
+    const response = await fetch("https://login.live.com/oauth20_connect.srf", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: "client_id=0000000040159362&scope=service::user.auth.xboxlive.com::MBI_SSL&response_type=device_code",
+    });
+
+    if (!response.ok) {
+        return undefined;
+    }
+
+    const json = (await response.json()) as IDeviceCodeResponse;
+    const { verification_uri, device_code, expires_in, user_code } = json;
+    console.log(user_code);
+
+    const height = 600;
+    const width = 500;
+    const authWindow = new BrowserWindow({ width: width, height: height, maxWidth: width, maxHeight: height, minWidth: width, minHeight: height });
+    authWindow.removeMenu();
+    authWindow.loadURL(verification_uri);
+
+    let secondsPassed = 0;
+    const interval = setInterval(() => {
+        secondsPassed++;
+    }, 1000);
+
+    let refreshToken: string | undefined = undefined;
+
+    while (secondsPassed < expires_in * 1000) {
+        const oauthResponse = await fetch("https://login.live.com/oauth20_token.srf", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `device_code=${device_code}&client_id=0000000040159362&grant_type=device_code`,
+        });
+
+        if (oauthResponse.ok) {
+            const oauthJson = (await oauthResponse.json()) as IOAuthResponse;
+            refreshToken = oauthJson.refresh_token;
+            break;
+        }
+
+        await sleep(1500);
+    }
+
+    clearInterval(interval);
+    authWindow.destroy();
+
+    return refreshToken;
+}
--- src/backend/events/responses/profile.ts
+++ src/backend/events/responses/profile.ts
@@ -2,7 +2,7 @@ import { addProfile, removeProfile, editProfile } from "../../managers/profile/r
 
 export async function addProfileEventResponse(profile: string, index: number, beforeName?: string) {
     if (beforeName === undefined) await addProfile(profile, index);
-    if (beforeName !== undefined) await editProfile(profile, index, beforeName)
+    if (beforeName !== undefined) await editProfile(profile, index, beforeName);
 }
 
 export async function removeProfileEventResponse(profile: string) {
